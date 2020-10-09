3a83813ead35b431fb76ddfc06d467868f679706
khareemah <ajimobikareemah@gmail.com>
2020-10-09 05:28:25 -0700

    initial commit

---
+++ app.js
@@ -0,0 +1,15 @@
+const links = document.querySelector(".nav ul").children;
+const nav = document.querySelector(".nav");
+const navOverlay = document.querySelector(".nav-overlay");
+const navToggle = document.querySelector(".nav-toggle");
+for (let i = 0; i < links.length; i++) {
+  links[i].addEventListener("click", function() {
+    for (let j = 0; j < links.length; j++) {
+      links[j].classList.remove("active");
+    }
+    links[i].classList.add("active");
+  });
+}
+navToggle.addEventListener("click", function() {
+  nav.classList.toggle("open");
+});
